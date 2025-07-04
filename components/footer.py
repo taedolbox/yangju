fd3b098d0438b5d428a6a971ea0d5398b2685828
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-07-05 07:30:18 +0900

    Update footer.py

--- components/footer.py
+++ components/footer.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 def render_footer():
     st.markdown("---")
-    st.markdown("🔎 [고용센터 찾기](https://www.ei.go.kr/ei/eih/eg/pb/pbPersonBnef/retrievePbMplymnCnterList.do)")
-
+    st.caption("ⓒ 2025 실업급여 도우미는 도움을 드리기 위한 참고용입니다. 실제 가능 여부는 고용센터 판단을 따릅니다.")
+    st.markdown("[📌 고용센터 찾기](https://www.work24.go.kr/cm/c/d/0190/retrieveInstSrchLst.do)")
