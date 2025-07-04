f0c4d9042c4d24f60a216def364b011065508ce1
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-07-04 14:59:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 from app.daily_worker_eligibility import daily_worker_eligibility_app
 from app.early_reemployment import early_reemployment_app
 from app.questions import (
-    get_employment_questions,
+    get_self_employment_questions,
     get_daily_worker_eligibility_questions
 )
 
