59cbc7ff173a216385eea2d9deb2b392747cd782
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-07-05 09:11:00 +0900

    Delete components directory

--- components/footer.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def render_footer():
-    st.markdown("---")
-    st.caption("ⓒ 2025 실업급여 도우미는 도움을 드리기 위한 참고용입니다. 실제 가능 여부는 고용센터 판단을 따릅니다.")
-    st.markdown("[📌 고용센터 찾기](https://www.work24.go.kr/cm/c/d/0190/retrieveInstSrchLst.do)")
