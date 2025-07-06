a6afb40e99d6466f31317194aa8464c8c1f67185
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-07-07 08:57:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import sys
-import os
-sys.path.append(os.path.abspath('.'))
 import streamlit as st
 import os
-from app.daily_worker_eligibility import daily_worker_eligibility_app
+import sys
 
+st.write("현재 작업 경로:", os.getcwd())
+st.write("sys.path:", sys.path)
 st.write("app 폴더 경로:", os.path.abspath("app"))
 st.write("app 폴더 파일 리스트:", os.listdir("app"))
+
+try:
+    from app.daily_worker_eligibility import daily_worker_eligibility_app
+    st.write("임포트 성공:", daily_worker_eligibility_app)
+except Exception as e:
+    st.write("임포트 실패:", e)
