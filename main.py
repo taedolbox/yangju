0be3c5acf3e69f6f32a0e868d04a005ece9aab59
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-07-07 08:50:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sys
-import streamlit as st
+import os
 
-st.write("sys.path:", sys.path)
-
-import app
-st.write("dir(app):", dir(app))
+st.write("app 폴더 경로:", os.path.abspath("app"))
+st.write("app 폴더 파일 리스트:", os.listdir("app"))
