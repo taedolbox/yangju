f5a1b8251082e1a40a33696808158ef0e2ea2f02
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-07-07 08:53:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+from app.daily_worker_eligibility import daily_worker_eligibility_app
 
 st.write("app 폴더 경로:", os.path.abspath("app"))
 st.write("app 폴더 파일 리스트:", os.listdir("app"))
