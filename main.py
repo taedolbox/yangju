6c585ca13c8565fd029db4097c6a907440b90a5f
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-07-07 10:41:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
+import streamlit as st
 
-print("현재 작업 경로:", os.getcwd())
-print("app 폴더 존재 여부:", os.path.isdir("app"))
-print("app/__init__.py 존재 여부:", os.path.isfile("app/__init__.py"))
-print("app/daily_worker_eligibility.py 존재 여부:", os.path.isfile("app/daily_worker_eligibility.py"))
+st.write("현재 작업 경로:", os.getcwd())
+st.write("app 폴더 존재 여부:", os.path.isdir("app"))
+st.write("app/__init__.py 존재 여부:", os.path.isfile("app/__init__.py"))
+st.write("app/daily_worker_eligibility.py 존재 여부:", os.path.isfile("app/daily_worker_eligibility.py"))
