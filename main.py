b4450b8d900440dda1b5021fceda4648acad3b4f
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-07-07 10:19:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import streamlit as st
 from app.daily_worker_eligibility import daily_worker_eligibility_app
+from app.early_reemployment import early_reemployment_app
 
 def main():
     st.set_page_config(
         page_title="실업급여 지원 시스템",
         page_icon="💼",
-        layout="centered",
+        layout="centered"
     )
 
     st.title("💼 실업급여 지원 시스템")
 
     menu = st.radio(
         "메뉴 선택",
-        ["일용직(건설일용포함)"],
+        ["조기재취업수당", "일용직(건설일용포함)"],
         horizontal=True
     )
 
-    if menu == "일용직(건설일용포함)":
+    if menu == "조기재취업수당":
+        early_reemployment_app()
+    elif menu == "일용직(건설일용포함)":
         daily_worker_eligibility_app()
 
 if __name__ == "__main__":
