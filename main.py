d2576338db6f21deb6fcc5ec5c58a516fcf618e7
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-07-07 10:48:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ sys.path.append(os.path.abspath('.'))
 try:
     from app.daily_worker_eligibility import daily_worker_eligibility_app
     st.success("임포트 성공!")
-    st.write("함수 타입:", type(daily_worker_eligibility_app))
 except Exception as e:
     st.error("임포트 실패:")
     st.error(str(e))
